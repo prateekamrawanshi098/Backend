e7fa945223b057205bca4d3ebf37d7acf547b433
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-03-03 22:29:48 +0530

    march 3

--- day8/backend/src/App.js
+++ day8/backend/src/App.js
@@ -1,58 +1,60 @@
 const express = require("express");
-const noteModel = require("./models/Note.model")
-const cors=require("cors")
-
+const noteModel = require("./models/Note.model");
+const cors = require("cors");
+const path = require("path");
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public/dist"))
 
-app.post("/notes", async(req, res) => {
-    
-    const { title, description } = req.body;
+app.post("/notes", async (req, res) => {
+  const { title, description } = req.body;
 
-    const note =await noteModel.create({
-         title,description
-    })
+  const note = await noteModel.create({
+    title,
+    description,
+  });
 
-    res.status(201).json({
-        message: "Note created succssfully",
-        note
-    })
-})
+  res.status(201).json({
+    message: "Note created succssfully",
+    note,
+  });
+});
 
-app.get("/notes", async(req, res)=>{
-    
-    const notes =await noteModel.find()
-    
-    res.status(200).json({
-        message: "notes Feched succesfully",
-        notes
-    })
-})
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
 
-app.delete("/notes/:id", async(req, res) => {
-    const id = req.params.id
-    await noteModel.findByIdAndDelete(id)
-    const note=await noteModel.find()
+  res.status(200).json({
+    message: "notes Feched succesfully",
+    notes,
+  });
+});
 
-    res.status(200).json({
-        message: "note deleted successlly "
-        ,note
-    })
-})
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findByIdAndDelete(id);
+  const note = await noteModel.find();
 
-app.patch("/notes/:id", async(req, res) => {
-    const id=req.params.id
-    const { description } = req.body;
-    
-    await noteModel.findByIdAndUpdate(id, { description });
-    
-    res.status(200).json({
-        message: "Note updated successfully"
-    })
+  res.status(200).json({
+    message: "note deleted successlly ",
+    note,
+  });
+});
 
-})
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
 
+  await noteModel.findByIdAndUpdate(id, { description });
 
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
+console.log(__dirname);
 
-module.exports=app
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/dist/index.html"));
+});
+
+module.exports = app;
--- day8/frontend/class0/src/App.jsx
+++ day8/frontend/class0/src/App.jsx
@@ -1,22 +1,72 @@
-import React, { useState } from "react";
-import axios from 'axios'
-const App = () => {
-  const [notes, setnotes] = useState([
-   
-  ]);
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-  axios.get('http://localhost:3000/notes')
-    .then((res) => {
-      setnotes(res.data.notes)
-  })
+const App = () => {
+  const [notes, setnotes] = useState([]);
+ 
+  console.log("hello integratrion");
+  
+
+
+  function fetchNotes() {
+    axios.get("http://localhost:3000/notes").then((res) => {
+      setnotes(res.data.notes);
+    });
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault()      
+
+    const { title, description } = e.target.elements
+    
+    console.log(title.value,description.value);
+    
+    axios.post("http://localhost:3000/notes",{
+      title: title.value,
+      description:description.value
+    })
+
+      .then(res => {
+        console.log(res.data);
+        fetchNotes()
+      
+    })
+
+  }
+
+  function deleteHandle(id) {
+    console.log(id);
+
+    axios.delete("http://localhost:3000/notes/" + id)
+      .then(res => {
+        console.log(res.data);
+        fetchNotes()
+      })
+    
+  }
+
 
   return (
     <div>
+
+      <form className="form-note" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter title" name="title" />
+        <input type="text" placeholder="Enter description" name="description" id="" />
+        <button>Create Note</button>
+      </form>
+
       <div className="notes">
         {notes.map((elem, index) => (
           <div className="note" key={index}>
             <h1>{elem.title}</h1>
             <p>{elem.description}</p>
+            <button onClick={() => {
+              deleteHandle(elem._id)
+            }}>delete</button>
           </div>
         ))}
       </div>
