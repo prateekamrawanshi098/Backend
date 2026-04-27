c236071b2959eb770be3959309063458b1120068
Prateek Amrawanshi <prateekamrawanshimk7193@gmail.com@gmail.com>
2026-04-27 10:30:24 +0530

    27 April

--- day-13-gen-ai/User-Input.js
+++ day-13-gen-ai/User-Input.js
@@ -2,6 +2,21 @@ import "dotenv/config";
 import readline from "readline";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { HumanMessage } from "@langchain/core/messages";
+import * as z from "zod";
+import { tool } from "@langchain/core/tools";
+import { createToolCallingAgent, AgentExecutor } from "langchain/agents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { sendEmail } from "./mail.service.js";
+
+const sendEmailTool = tool(sendEmail, {
+  name: "email_sender",
+  description: "used to send email",
+  schema: z.object({
+    to: z.string().email().describe("The recipient email address"),
+    html: z.string().describe("This is for html content"),
+    subject: z.string().describe("The subject of the mail"),
+  }),
+});
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,7 +28,24 @@ const model = new ChatMistralAI({
   temperature: 0,
 });
 
-const messages=[]
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant. Use tools when needed."],
+  ["human", "{input}"],
+  ["placeholder", "{agent_scratchpad}"],
+]);
+
+const agent = createToolCallingAgent({
+  llm: model,
+  tools: [sendEmailTool],
+  prompt,
+});
+
+const agentExecutor = new AgentExecutor({
+  agent,
+  tools: [sendEmailTool],
+});
+
+const messages = [];
 
 function askQuestion(question) {
   return new Promise((resolve) => {
@@ -23,17 +55,19 @@ function askQuestion(question) {
 
 async function main() {
   try {
-      while (true) {
-        const name = await askQuestion("You : ");
+    while (true) {
+      const name = await askQuestion("You : ");
 
-        messages.push(new HumanMessage(name))
+      messages.push(new HumanMessage(name));
 
-        const response = await model.invoke(messages);
+      const response = await agentExecutor.invoke({
+        input: name,
+      });
 
-        messages.push(response)
+      
 
-        console.log(response.content);
-   }
+      console.log(response.output);
+    }
   } catch (error) {
     console.error("Error:", error.message);
   } finally {
---
+++ day-13-gen-ai/mail.service.js
@@ -0,0 +1,43 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+function createOauth2Client() {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground",
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  });
+
+  return oauth2Client;
+}
+
+export async function sendEmail({ to, subject, html,text="" }) {
+  const oauth2Client = createOauth2Client();
+  const accessToken = await oauth2Client.getAccessToken();
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.GOOGLE_USER,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+
+  await transporter.sendMail({
+    from: process.env.GOOGLE_USER,
+    to,
+    subject,
+    html,
+    text
+  });
+
+  console.log("✅ Email sent");
+}
